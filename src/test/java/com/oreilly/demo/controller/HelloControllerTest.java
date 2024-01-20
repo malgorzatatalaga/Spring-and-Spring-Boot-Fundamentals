130b97d11babf20348ccde2d38a5dfea74ab04cb
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2024-01-20 21:17:55 +0100

    testing controllers

---
+++ src/test/java/com/oreilly/demo/controller/HelloControllerTest.java
@@ -0,0 +1,20 @@
+package com.oreilly.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+import org.springframework.validation.support.BindingAwareModelMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloControllerTest {
+    @Test
+    public void sayHello() {
+        HelloController controller = new HelloController();
+        Model model = new BindingAwareModelMap();
+        String result = controller.sayHello("World", model);
+        assertAll(
+                () -> assertEquals("World", model.getAttribute("user")),
+                () -> assertEquals("hello", result)
+        );
+    }
+}
