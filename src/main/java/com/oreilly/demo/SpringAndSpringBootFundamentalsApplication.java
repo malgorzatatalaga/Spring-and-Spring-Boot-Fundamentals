5c04989bd92a390e957ebfa2b26eeba0377fc72f
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2024-01-20 13:11:18 +0100

    initial commit

---
+++ src/main/java/com/oreilly/demo/SpringAndSpringBootFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.oreilly.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndSpringBootFundamentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndSpringBootFundamentalsApplication.class, args);
+    }
+
+}
