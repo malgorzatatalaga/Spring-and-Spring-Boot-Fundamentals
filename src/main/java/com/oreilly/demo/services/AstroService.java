12c9b7c155b2e748787f21350f5e8c2938a22a80
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2024-01-22 20:02:10 +0100

    Using the Reactive WebClient Class

--- src/main/java/com/oreilly/demo/services/AstroService.java
+++ src/main/java/com/oreilly/demo/services/AstroService.java
@@ -3,20 +3,36 @@ package com.oreilly.demo.services;
 import com.oreilly.demo.json.AstroResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
 
 @Service
 public class AstroService {
     private final RestTemplate restTemplate;
+    private final WebClient client;
 
     @Autowired
-    public AstroService(RestTemplateBuilder restTemplateBuilder) {
+    public AstroService(RestTemplateBuilder restTemplateBuilder,
+                        WebClient.Builder webfluxBuilder) {
         restTemplate = restTemplateBuilder.build();
+        client = webfluxBuilder.baseUrl("http://api.open-notify.org").build();
     }
 
     public AstroResult getAstronauts() {
         String url = "http://api.open-notify.org/astros.json";
         return restTemplate.getForObject(url, AstroResult.class);
+    }
+
+    public AstroResult getAstronautsWebFlux() {
+        return client.get()
+                .uri("astros.json")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(AstroResult.class)
+                .block(Duration.ofSeconds(2));
     }
 }
--- src/test/java/com/oreilly/demo/services/AstroServiceTest.java
+++ src/test/java/com/oreilly/demo/services/AstroServiceTest.java
@@ -27,4 +27,17 @@ public class AstroServiceTest {
                 () -> assertEquals(number, people.size())
         );
     }
+
+    @Test
+    void getAstronautsWebFlux() {
+        AstroResult result = service.getAstronautsWebFlux();
+        int number = result.number();
+        System.out.println("There are " + number + " people in space");
+        List<Assignment> people = service.getAstronauts().people();
+        people.forEach(System.out::println);
+        assertAll(
+                () -> assertTrue(number >= 0),
+                () -> assertEquals(number, people.size())
+        );
+    }
 }
