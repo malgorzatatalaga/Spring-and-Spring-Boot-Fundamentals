19b30ec428ad45386a5439a851cec7167678008c
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2024-01-22 17:48:12 +0100

    Creating a RESTful Web Service

---
+++ src/main/java/com/oreilly/demo/json/Greeting.java
@@ -0,0 +1,20 @@
+package com.oreilly.demo.json;
+
+public class Greeting {
+    private final String message;
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
